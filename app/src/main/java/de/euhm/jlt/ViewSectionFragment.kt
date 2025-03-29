f5e8a8f5e2e6e5a024832c11819d8c019c9fa3fd
Holger Müller <github@euhm.de>
2025-03-29 21:58:42 +0100

    updated header

--- app/src/main/java/de/euhm/jlt/ViewSectionFragment.kt
+++ app/src/main/java/de/euhm/jlt/ViewSectionFragment.kt
@@ -1,11 +1,11 @@
-/*
- * @file ViewSectionFragment.java
- * @author Holger Mueller
+/**
+ * @file ViewSectionFragment.kt
  *
  * based on http://www.vogella.com/tutorials/AndroidSQLite/article.html#databasetutorial
+ * Converted from Java to Kotlin by Android Studio Meerkat | 2024.3.1 in March 2025
  *
- * Licensed under the Apache License, Version 2.0 (the "License")
- * http://www.apache.org/licenses/LICENSE-2.0
+ * MIT License
+ * Copyright (c) 2014-2025 Holger Mueller
  */
 package de.euhm.jlt
 
