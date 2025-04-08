7887b09671f0b1cb46987f956de0690f9c386e34
Holger Müller <github@euhm.de>
2025-04-08 21:36:31 +0200

    switched to androidx.preference.PreferenceManager

--- app/src/main/java/de/euhm/jlt/MainActivity.kt
+++ app/src/main/java/de/euhm/jlt/MainActivity.kt
@@ -22,7 +22,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.preference.PreferenceManager
 import android.provider.Settings
 import android.util.Log
 import android.view.Menu
@@ -44,6 +43,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentTransaction
+import androidx.preference.PreferenceManager
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
